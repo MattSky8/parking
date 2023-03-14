1be710aac0ae6e97a69d8681098cd06343afa288
MattSky8 <112981773+MattSky8@users.noreply.github.com>
2023-03-14 12:12:25 -0500

    file reformats

--- ticketLog/views.py
+++ ticketLog/views.py
@@ -2,34 +2,37 @@ from django.shortcuts import render
 from django.views import View
 from datetime import datetime
 from ticketLog.models import Sections, Days, Ticket
+
+
 # Create your views here.
 class Home(View):
-    def get(self,request):
+    def get(self, request):
         print(Days.choices)
-        return render(request,"home.html",{"days":Days.choices,"sections":Sections.choices})
+        return render(request, "home.html", {"days": Days.choices, "sections": Sections.choices})
 
-    def post(self,request):
+    def post(self, request):
         print(Days.choices)
-        #extract form data from POST
-        #convert datetime-local string to a Python datetime
-        datetime.strptime('2015-01-02T00:00', '%Y-%m-%dT%H:%M') #replace the first argument
-        #instantiate and save a Ticket
-            #to get day of the week, use strftime("%A")
-            #like
-            #a = datetime.now()
-            #print(a.strftime("%A"))
-            #also you can use the class Days like a dictionary
-            #print(Days["Monday"]) #prints "M", just like print(Days.Monday)
-        #render a response, identical to the page rendered by get
-        return render(request,"home.html",{"days":Days.choices,"sections":Sections.choices})
+        # extract form data from POST
+        # convert datetime-local string to a Python datetime
+        datetime.strptime('2015-01-02T00:00', '%Y-%m-%dT%H:%M')  # replace the first argument
+        # instantiate and save a Ticket
+        # to get day of the week, use strftime("%A")
+        # like
+        # a = datetime.now()
+        # print(a.strftime("%A"))
+        # also you can use the class Days like a dictionary
+        # print(Days["Monday"]) #prints "M", just like print(Days.Monday)
+        # render a response, identical to the page rendered by get
+        return render(request, "home.html", {"days": Days.choices, "sections": Sections.choices})
+
 
 class History(View):
-    def get(self,request):
-        return render(request,"history.html",{"days":Days.choices,"sections":Sections.choices})
+    def get(self, request):
+        return render(request, "history.html", {"days": Days.choices, "sections": Sections.choices})
 
-    def post(self,request):
-    #extract day and section from POST
-    #query (filter) for tickets
+    def post(self, request):
+        # extract day and section from POST
+        # query (filter) for tickets
         Ticket.objects.filter(dayOfWeek="U").values()
-    #render a response (with a table of matching tickets)
-        return render(request,"history.html",{"days":Days.choices,"sections":Sections.choices})
+        # render a response (with a table of matching tickets)
+        return render(request, "history.html", {"days": Days.choices, "sections": Sections.choices})
