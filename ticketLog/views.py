14b31d613d42fb7509056512f9c6be2168dc5026
MattSky8 <112981773+MattSky8@users.noreply.github.com>
2023-03-28 10:13:09 -0500

    Previous work on lab 8

--- ticketLog/views.py
+++ ticketLog/views.py
@@ -13,23 +13,15 @@ class Home(View):
     def post(self, request):
         print(Days.choices)
         # extract form data from POST
-        datestr = request.POST["dateTime"].__str__()
+        datestr = request.POST["dateTime"]
         if len(datestr) > 0:
             # convert datetime-local string to a Python datetime
             dt = datetime.strptime(datestr, '%Y-%m-%dT%H:%M')  # replace the first argument
-            # instantiate and save a Ticket
             ticket = Ticket()
             ticket.dateTime = dt
             ticket.dayOfWeek = dt.strftime('%A')
             ticket.section = request.POST["section"]
             ticket.save()
-            # to get day of the week, use strftime("%A")
-            # like
-            # a = datetime.now()
-            # print(a.strftime("%A"))
-            # also you can use the class Days like a dictionary
-            # print(Days["Monday"]) #prints "M", just like print(Days.Monday)
-        # render a response, identical to the page rendered by get
         return render(request, "home.html", {"days": Days.choices, "sections": Sections.choices})
 
 
@@ -45,6 +37,8 @@ class History(View):
             section = request.POST["section"]
             # query (filter) for tickets
             results = list(Ticket.objects.filter(dayOfWeek=day, section=section))
-            tickets = map(lambda ticket: [f"{ticket.section}", f"{ticket.dateTime:%H:%M}", f"{ticket.dateTime:%d/%m/%yY}"])
+            tickets = list(map(lambda ticket:
+                          [f"{ticket.section}", f"{ticket.dateTime:%H:%M}", f"{ticket.dateTime:%d/%m/%yY}"],
+                          results))
         # render a response (with a table of matching tickets)
         return render(request, "history.html", {"days": Days.choices, "sections": Sections.choices, "tickets": tickets})
